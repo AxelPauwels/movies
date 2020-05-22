80b38be26f1a4a41e292e0629a5e82a35d963871
Axel Pauwels <axelpauwels.be@gmail.com>
2020-05-22 19:52:23 +0200

    MOV-001 Updating ico and website title

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" type="image/png" href="<?php echo base_url(); ?>/Raspi.png">
+    <link rel="icon" type="image/png" href="<?php echo base_url(); ?>/moviesIcon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css">
     <link rel="stylesheet"
